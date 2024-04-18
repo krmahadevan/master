e3b5df3e3e6e5ee8ddd14de89f41535ef48342d7
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2024-04-18 20:03:20 +0530

    More changes

--- src/main/java/com/rationaleemotions/master/api/IngressController.java
+++ src/main/java/com/rationaleemotions/master/api/IngressController.java
@@ -1,6 +1,7 @@
 package com.rationaleemotions.master.api;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/master")
@@ -18,12 +21,22 @@ public class IngressController {
     private final RestClient client;
 
     @GetMapping(path = "/greet", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Greeting> greeting() {
-        Greeting downstream = Optional.ofNullable(client.get().uri("/worker/greet").retrieve().body(Greeting.class))
-                .orElse(new Greeting("UNKNOWN"));
-        return ResponseEntity.ok(new Greeting("Hello " + downstream.message()));
+    public ResponseEntity<GreetingOutBound> greeting() {
+        List<String> userAgent = new ArrayList<>();
+        GreetingInBound reply = client.get()
+                .uri("/worker/greet").exchange((clientRequest, clientResponse) -> {
+                    HttpHeaders headers = clientResponse.getHeaders();
+                    userAgent.addAll(headers.getOrEmpty("User-Agent"));
+                    return Objects.requireNonNull(clientResponse.bodyTo(GreetingInBound.class));
+                });
+
+        return ResponseEntity.ok(new GreetingOutBound("Hello " + reply.message(),
+                String.join(", ", userAgent)));
     }
 
-    public record Greeting(String message) {
+    public record GreetingInBound(String message) {
+    }
+
+    public record GreetingOutBound(String message, String userAgent) {
     }
 }
